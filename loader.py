f016188696973879c38ca6ed83b09bd51d5237c7
Sergeich7 <108126710+Sergeich7@users.noreply.github.com>
2022-11-14 18:09:53 +0600

    Сontainerization added

--- loader.py
+++ loader.py
@@ -1,13 +1,7 @@
 import os
 
-from dotenv import load_dotenv
-
 from aiogram import Bot, types, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
 
 bot = Bot(os.environ.get('TOKEN'), parse_mode=types.ParseMode.HTML)
 
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,14 +1,13 @@
+import os
 import logging
 
 from aiogram import Dispatcher
 
-from config import admins
-
 async def on_startup_notify(dp: Dispatcher):
-    for admin in admins:
+    for admin in os.environ.get('ADMINS').split(','):
         try:
             text = "Бот запущен и готов к работе"
             await dp.bot.send_message(chat_id=admin, text=text)
         except Exception as err:
             logging.exception(err)
-            
+
